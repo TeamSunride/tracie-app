ecc543cf464cada892184bc6a0f4665131f18d38
Starman <30315137+Starman3787@users.noreply.github.com>
2024-12-03 17:34:53 +0000

    bluetooth stuff

--- app/App.tsx
+++ app/App.tsx
@@ -5,9 +5,15 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
+  Alert,
   Button,
+  FlatList,
+  NativeEventEmitter,
+  NativeModules,
+  PermissionsAndroid,
+  Platform,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -16,15 +22,17 @@ import {
   View,
 } from 'react-native';
 import '../global.css';
-import { broadcast, setCompanyId } from 'react-native-ble-advertise';
+import BleManager from 'react-native-ble-manager';
+// import { broadcast, setCompanyId } from 'react-native-ble-advertise';
 
-const uuid = '44C13E43-097A-9C9F-537F-5666A6840C08';
-const major = parseInt('CD00', 16);
-const minor = parseInt('0003', 16);
+// const uuid = '44C13E43-097A-9C9F-537F-5666A6840C08';
+// const major = parseInt('CD00', 16);
+// const minor = parseInt('0003', 16);
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import ScanDevicesScreen from './components/ScanDevices';
+
+console.log(NativeModules);
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -32,6 +40,229 @@ function App(): React.JSX.Element {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  // const [peripherals, setPeripherals] = useState([]);
+  // const [connectedPeripheral, setConnectedPeripheral] = useState(null);
+  // const [scanning, setScanning] = useState(false);
+  // const [error, setError] = useState(null);
+
+  // const init = () => {
+  //   // Initialize BleManager module
+  //   BleManager.start({showAlert: false});
+
+  //   // Create an event emitter instance
+  //   const bleManagerEmitter = new NativeEventEmitter(BleManager);
+
+  //   // Add event listeners for BLE events
+  //   bleManagerEmitter.addListener(
+  //     'BleManagerDiscoverPeripheral',
+  //     peripheral => {
+  //       console.log(peripheral);
+  //       // Update peripherals array with scanned device
+  //       setPeripherals(prevPeripherals => [...prevPeripherals, peripheral]);
+  //     },
+  //   );
+
+  //   bleManagerEmitter.addListener('BleManagerStopScan', () => {
+  //     // Update scanning state
+  //     setScanning(false);
+  //   });
+
+  //   bleManagerEmitter.addListener('BleManagerConnectPeripheral', peripheral => {
+  //     // Update connected peripheral object
+  //     setConnectedPeripheral(peripheral);
+  //   });
+
+  //   bleManagerEmitter.addListener(
+  //     'BleManagerDisconnectPeripheral',
+  //     peripheral => {
+  //       // Update connected peripheral object
+  //       setConnectedPeripheral(null);
+  //     },
+  //   );
+
+  //   bleManagerEmitter.addListener(
+  //     'BleManagerDidUpdateValueForCharacteristic',
+  //     ({value, peripheral, characteristic, service}) => {
+  //       // Display notification message
+  //       console.log(
+  //         `Received notification from ${peripheral} - ${service} - ${characteristic}: ${value}`,
+  //       );
+  //     },
+  //   );
+  // };
+
+  // const scan = () => {
+  //   // Check if scanning is already in progress
+  //   if (scanning) {
+  //     return;
+  //   }
+
+  //   // Clear peripherals array
+  //   setPeripherals([]);
+
+  //   // Start scanning for devices that match a given service UUID filter
+  //   BleManager.scan(["2a37"], 5, true)
+  //     .then(() => {
+  //       // Update scanning state
+  //       setScanning(true);
+  //     })
+  //     .catch(err => {
+  //       // Update error message
+  //       setError(err.message);
+  //     });
+  // };
+
+  // const stopScan = () => {
+  //   // Stop scanning for devices
+  //   BleManager.stopScan()
+  //     .then(() => {
+  //       // Update scanning state
+  //       setScanning(false);
+  //     })
+  //     .catch(err => {
+  //       // Update error message
+  //       setError(err.message);
+  //     });
+  // };
+
+  // const connect = id => {
+  //   // Connect to a device by its ID
+  //   BleManager.connect(id)
+  //     .then(() => {
+  //       // Retrieve device information
+  //       BleManager.retrieveServices(id)
+  //         .then(peripheralInfo => {
+  //           // Update connected peripheral object with device information
+  //           setConnectedPeripheral(peripheralInfo);
+  //         })
+  //         .catch(err => {
+  //           // Update error message
+  //           setError(err.message);
+  //         });
+  //     })
+  //     .catch(err => {
+  //       // Update error message
+  //       setError(err.message);
+  //     });
+  // };
+
+  // const disconnect = () => {
+  //   // Check if there is a connected device
+  //   if (connectedPeripheral) {
+  //     // Disconnect from the device by its ID
+  //     BleManager.disconnect(connectedPeripheral.id)
+  //       .then(() => {
+  //         // Update connected peripheral object
+  //         setConnectedPeripheral(null);
+  //       })
+  //       .catch(err => {
+  //         // Update error message
+  //         setError(err.message);
+  //       });
+  //   }
+  // };
+
+  // const read = (serviceUUID, characteristicUUID) => {
+  //   // Check if there is a connected device
+  //   if (connectedPeripheral) {
+  //     // Read a characteristic value from the device by its service UUID and characteristic UUID
+  //     BleManager.read(connectedPeripheral.id, serviceUUID, characteristicUUID)
+  //       .then(value => {
+  //         // Display read value
+  //         console.log(
+  //           `Read value from ${connectedPeripheral.id} - ${serviceUUID} - ${characteristicUUID}: ${value}`,
+  //         );
+  //       })
+  //       .catch(err => {
+  //         // Update error message
+  //         setError(err.message);
+  //       });
+  //   }
+  // };
+
+  // const write = (serviceUUID, characteristicUUID, value) => {
+  //   // Check if there is a connected device
+  //   if (connectedPeripheral) {
+  //     // Write a characteristic value to the device by its service UUID and characteristic UUID
+  //     BleManager.write(
+  //       connectedPeripheral.id,
+  //       serviceUUID,
+  //       characteristicUUID,
+  //       value,
+  //     )
+  //       .then(() => {
+  //         // Display confirmation message
+  //         console.log(
+  //           `Wrote value to ${connectedPeripheral.id} - ${serviceUUID} - ${characteristicUUID}: ${value}`,
+  //         );
+  //       })
+  //       .catch(err => {
+  //         // Update error message
+  //         setError(err.message);
+  //       });
+  //   }
+  // };
+
+  // const subscribe = (serviceUUID, characteristicUUID) => {
+  //   // Check if there is a connected device
+  //   if (connectedPeripheral) {
+  //     // Subscribe to notifications or indications from a characteristic of the device by its service UUID and characteristic UUID
+  //     BleManager.startNotification(
+  //       connectedPeripheral.id,
+  //       serviceUUID,
+  //       characteristicUUID,
+  //     )
+  //       .then(() => {
+  //         // Display confirmation message
+  //         console.log(
+  //           `Subscribed to ${connectedPeripheral.id} - ${serviceUUID} - ${characteristicUUID}`,
+  //         );
+  //       })
+  //       .catch(err => {
+  //         // Update error message
+  //         setError(err.message);
+  //       });
+  //   }
+  // };
+
+  // const unsubscribe = (serviceUUID, characteristicUUID) => {
+  //   // Check if there is a connected device
+  //   if (connectedPeripheral) {
+  //     // Unsubscribe from notifications or indications from a characteristic of the device by its service UUID and characteristic UUID
+  //     BleManager.stopNotification(
+  //       connectedPeripheral.id,
+  //       serviceUUID,
+  //       characteristicUUID,
+  //     )
+  //       .then(() => {
+  //         // Display confirmation message
+  //         console.log(
+  //           `Unsubscribed from ${connectedPeripheral.id} - ${serviceUUID} - ${characteristicUUID}`,
+  //         );
+  //       })
+  //       .catch(err => {
+  //         // Update error message
+  //         setError(err.message);
+  //       });
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   // Initialize BLE manager when component mounts
+  //   init();
+
+  //   // return () => {
+  //   //   // Remove event listeners when component unmounts
+  //   //   bleManagerEmitter.removeAllListeners('BleManagerDiscoverPeripheral');
+  //   //   bleManagerEmitter.removeAllListeners('BleManagerStopScan');
+  //   //   bleManagerEmitter.removeAllListeners('BleManagerConnectPeripheral');
+  //   //   bleManagerEmitter.removeAllListeners('BleManagerDisconnectPeripheral');
+  //   //   bleManagerEmitter.removeAllListeners(
+  //   //     'BleManagerDidUpdateValueForCharacteristic',
+  //   //   );
+  //   // };
+  // }, []);
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -53,13 +284,80 @@ function App(): React.JSX.Element {
           <Text className="text-3xl text-secondary text-center font-bold italics">
             Tracie App
           </Text>
-          <Button
+          <ScanDevicesScreen />
+          {/* <Button title='Check Permissions' onPress={checkPermissions} /> */}
+          {/* <Button
+            title={scanning ? 'Stop Scanning' : 'Start Scanning'}
+            onPress={scanning ? stopScan : scan}
+          />
+          <FlatList
+            data={peripherals}
+            keyExtractor={item => item.id}
+            renderItem={({item}) => (
+              <View style={{borderWidth: 1, margin: 5, padding: 5}}>
+                <Text className="text-5xl text-primary text-center font-bold">ID: {item.id}</Text>
+                <Text className="text-5xl text-primary text-center font-bold">Name: {item.name}</Text>
+                <Text className="text-5xl text-primary text-center font-bold">RSSI: {item.rssi}</Text>
+                <Text className="text-5xl text-primary text-center font-bold">Services: {item.advertising.serviceUUIDs}</Text>
+                <Button title="Connect" onPress={() => connect(item.id)} />
+              </View>
+            )}
+          />
+          {connectedPeripheral && (
+            <View style={{borderWidth: 1, margin: 5, padding: 5}}>
+              <Text>ID: {connectedPeripheral.id}</Text>
+              <Text>Name: {connectedPeripheral.name}</Text>
+              <Text>RSSI: {connectedPeripheral.rssi}</Text>
+              <Text>
+                Services: {JSON.stringify(connectedPeripheral.services)}
+              </Text>
+              <Text>
+                Characteristics:{' '}
+                {JSON.stringify(connectedPeripheral.characteristics)}
+              </Text>
+              <Button title="Disconnect" onPress={disconnect} />
+              <TextInput
+                placeholder="Enter service UUID"
+                onChangeText={text => setServiceUUID(text)}
+              />
+              <TextInput
+                placeholder="Enter characteristic UUID"
+                onChangeText={text => setCharacteristicUUID(text)}
+              />
+              <Button
+                title="Read"
+                onPress={() => read(serviceUUID, characteristicUUID)}
+              />
+              <Text>Read value: {readValue}</Text>{' '}
+              <TextInput
+                placeholder="Enter value to write"
+                onChangeText={text => setValue(text)}
+              />
+              <Button
+                title="Write"
+                onPress={() => write(serviceUUID, characteristicUUID, value)}
+              />
+              <Text>Write confirmation: {writeConfirmation}</Text>
+              <Switch
+                value={subscribed}
+                onValueChange={value =>
+                  value
+                    ? subscribe(serviceUUID, characteristicUUID)
+                    : unsubscribe(serviceUUID, characteristicUUID)
+                }
+              />
+              <Text>Subscription confirmation: {subscriptionConfirmation}</Text>
+              <Text>Notification message: {notificationMessage}</Text>{' '}
+            </View>
+          )}
+          {error && <Text style={{color: 'red'}}>{error}</Text>} */}
+          {/* <Button
             title="set company id"
             onPress={() => {
               setCompanyId(0x00E0);
             }}
-          />
-          <Button
+          /> */}
+          {/* <Button
             title="advertise"
             onPress={() => {
               broadcast(uuid, major, minor)
@@ -70,7 +368,7 @@ function App(): React.JSX.Element {
                   console.log('broadcast failed with: ' + error);
                 });
             }}
-          />
+          /> */}
         </View>
       </ScrollView>
     </SafeAreaView>
--- ios/mainTests/mainTests.m
+++ ios/mainTests/mainTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TracieAppTests : XCTestCase
+@interface mainTests : XCTestCase
 
 @end
 
-@implementation TracieAppTests
+@implementation mainTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {

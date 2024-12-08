21b8fc9df3d8cbd3798ba25f57d149f4d59a737c
Starman <30315137+Starman3787@users.noreply.github.com>
2024-12-08 15:25:49 +0000

    add animations and gradient packages

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,8 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-const { withNativeWind } = require("nativewind/metro");
+const {withNativeWind} = require('nativewind/metro');
+const {
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
 
 /**
  * Metro configuration
@@ -11,4 +14,6 @@ const config = {};
 
 const exportConfig = mergeConfig(getDefaultConfig(__dirname), config);
 
-module.exports = withNativeWind(exportConfig, { input: "./global.css" });
+const nativeWindConfig = withNativeWind(exportConfig, {input: './global.css'});
+
+module.exports = wrapWithReanimatedMetroConfig(nativeWindConfig);

394b17c70fa6b2350f51a9e1e82b0a787b5e7759
Starman <30315137+Starman3787@users.noreply.github.com>
2025-03-15 11:50:25 +0000

    add dart 3d model

--- app/components/History.tsx
+++ app/components/History.tsx
@@ -1,10 +1,10 @@
 import React, {Suspense, useEffect, useRef, useState} from 'react';
 import {Canvas, useFrame, useLoader} from '@react-three/fiber/native';
-import {Stars, useGLTF} from '@react-three/drei/native';
+import {PerspectiveCamera, Stars, useGLTF} from '@react-three/drei/native';
 import {View} from 'react-native';
+import {AmbientLight, DirectionalLight} from 'three';
 
-const Rocket = () => {
-
+const Rocket2 = (props) => {
   const rocketRef = useRef();
 
   // // Slowly spin the rocket
@@ -14,27 +14,30 @@ const Rocket = () => {
     }
   });
 
-  // // Load your 3D rocket model (ensure you have a GLTF model in your assets)
-  const {scene} = useGLTF(require("../../assets/rockets/cassini.glb"));
-  // const {scene} = useGLTFCustom(require("../../assets/rockets/satellite.glb")); //useLoader(GLTFLoader, require("../../assets/rockets/satellite.glb"));
-  return <primitive object={scene} ref={rocketRef} scale={0.1} />;
-
+  const { nodes, materials } = useGLTF(require('../../assets/rockets/DARTcoloured.glb'));
+  return (
+    <group {...props} dispose={null} ref={rocketRef}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.DART.geometry}
+        material={materials['AR3DMat PBR Shiny Chrome']}
+        scale={0.0005}
+      />
+    </group>
+  )
 };
 
 
-const SpinningRocketScreen = () => {
+const OtherSpinningRocketScreen = () => {
   return (
-    <View style={{flex: 1, backgroundColor: 'black'}}>
+    <View style={{flex: 1, backgroundColor: 'white'}}>
       <Canvas camera={{position: [0, 2, 5], fov: 100}}>
-        <ambientLight intensity={0.8} />
-        <directionalLight position={[2, 5, 5]} intensity={1} />
-        <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade={2} speed={1} />
-        <Suspense>
-          <Rocket />
-        </Suspense>
+        <ambientLight intensity={1} />
+        <Rocket2 />
       </Canvas>
     </View>
   );
 };
 
-export default SpinningRocketScreen;
+export default OtherSpinningRocketScreen;
